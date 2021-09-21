42e23fbbbb41fc5e16a56d5a47c75172e4242a3f
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-09-22 00:41:26 +0300

    Add a toggle method on click function.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,13 @@
 console.log("hello There");
 /* About  Section */
-
+$(document).ready(function() {
+    $(".shown").click(function() {
+        $(this).toggle();
+        $(this).siblings().toggle();
+    });
+    $(".hidden").click(function() {
+        $(this).toggle();
+        $(this).siblings().toggle();
+    })
+})
 /*  Contact  Section */
