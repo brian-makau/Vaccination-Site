2811b4a7f08c84a339394268379ea513927b7fa5
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-09-22 01:01:39 +0300

    Change text color.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,12 +2,12 @@ console.log("hello There");
 /* About  Section */
 $(document).ready(function() {
     $(".shown").click(function() {
-        $(this).toggle();
-        $(this).siblings().toggle();
+        $(this).toggle(2000);
+        $(this).siblings().toggle(2000);
     });
     $(".hidden").click(function() {
-        $(this).toggle();
-        $(this).siblings().toggle();
+        $(this).toggle(2000);
+        $(this).siblings().toggle(2000);
     })
 })
 /*  Contact  Section */
