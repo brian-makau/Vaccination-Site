7e11ac5cc3fe95d614f73117af3cbddf9d692705
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-09-22 09:16:38 +0300

    Remove toggle timeout and style h2 background.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,12 +2,12 @@ console.log("hello There");
 /* About  Section */
 $(document).ready(function() {
     $(".shown").click(function() {
-        $(this).toggle(2000);
-        $(this).siblings().toggle(2000);
+        $(this).toggle();
+        $(this).siblings().toggle();
     });
     $(".hidden").click(function() {
-        $(this).toggle(2000);
-        $(this).siblings().toggle(2000);
+        $(this).toggle();
+        $(this).siblings().toggle();
     })
 })
 /*  Contact  Section */
