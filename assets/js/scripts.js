e2857b904fa0761d74cdb26bbbd2fef51f6f4207
sling254 <mwanikidenis7@gmail.com>
2021-09-23 08:19:08 +0300

    code refactoring

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -37,56 +37,44 @@ class Store{
         localStorage.setItem("bookings",JSON.stringify(bookings));
     }
 }
-
-function individualFunc(e) {
-    e.preventDefault();
-    //collect user data
-    Id = $("#id").val();  
-    dob = $("#dob").val();
-    email =$("#email").val();
-    county = $("#county").val();
-    date = $("#date").val();
-    site = $("#location").val();
-    const individualbook = new Individualbook(Id,dob,email,county,date,site);
-    console.log(individualbook);
-    Store.addbooking(individualbook); 
-    console.log(individualbook); 
-    alert("Check your email we have send the summary of the appointment")
-    location.reload();
-     
-    
-}
-
-function companyFunc(e){
-    e.preventDefault();
-    //collect user data
-    companyName = $("#companyName").val();
-    timeslot = $("#timeslot").val();
-    people =$("#people").val();
-    companyaddress = $("#companyaddress").val();
-    companyemail = $("#companyemail").val();
-    alert("booking success")
-    const companybook = new Companybook(companyName,timeslot,people, companyaddress, companyemail);
-    console.log(companybook);
-    location.reload();
-}
-//add event listeners
-$("#indvidual-form").submit(individualFunc);
-$("#company-form").submit(companyFunc);
-
-//form reset
-
-
-
-/* About  Section */
 $(document).ready(function() {
-    $(".shown").click(function() {
-        $(this).toggle();
-        $(this).siblings().toggle();
-    });
-    $(".hidden").click(function() {
-        $(this).toggle();
-        $(this).siblings().toggle();
-    })
+
+    function individualFunc(e) {
+        e.preventDefault();
+        //collect user data
+        Id = $("#id").val();  
+        dob = $("#dob").val();
+        email =$("#email").val();
+        county = $("#county").val();
+        date = $("#date").val();
+        site = $("#location").val();
+        const individualbook = new Individualbook(Id,dob,email,county,date,site);
+        console.log(individualbook);
+        Store.addbooking(individualbook); 
+        console.log(individualbook); 
+        alert("Check your email we have send the summary of the appointment")
+        location.reload();      
+        
+    }
+
+    function companyFunc(e){
+        e.preventDefault();
+        //collect user data
+        companyName = $("#companyName").val();
+        timeslot = $("#timeslot").val();
+        people =$("#people").val();
+        companyaddress = $("#companyaddress").val();
+        companyemail = $("#companyemail").val();
+        alert("booking success")
+        const companybook = new Companybook(companyName,timeslot,people, companyaddress, companyemail);
+        console.log(companybook);
+        location.reload();
+    }
+    //add event listeners
+    $("#indvidual-form").submit(individualFunc);
+    $("#company-form").submit(companyFunc);
+    //footer year
+    let year = new Date().getFullYear();
+    $("#year").text(year);
+    
 })
-/*  Contact  Section */
